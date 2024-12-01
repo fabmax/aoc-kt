f8986db841f368060ac5ae962aacc32a0e2adef9
fabmax <fabmax.thiele@gmail.com>
2024-12-01 18:43:27 +0100

    unzip and groupingBy are much nicer!

--- src/main/kotlin/y2024/day01/Day01.kt
+++ src/main/kotlin/y2024/day01/Day01.kt
@@ -2,29 +2,19 @@ package y2024.day01
 
 import AocPuzzle
 import extractNumbers
-import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.abs
 
 fun main() = Day01.runAll()
 
 object Day01 : AocPuzzle<Int, Int>() {
     override fun solve1(input: List<String>): Int {
-        val left = mutableListOf<Int>()
-        val right = mutableListOf<Int>()
-        input.map { it.extractNumbers() }.forEach { (lt, rt) ->
-            left += lt
-            right += rt
-        }
+        val (left, right) = input.map { it.extractNumbers() }.map { (lt, rt) -> lt to rt }.unzip()
         return left.sorted().zip(right.sorted()).sumOf { (l, r) -> abs(l-r) }
     }
 
     override fun solve2(input: List<String>): Int {
-        val left = mutableListOf<Int>()
-        val right = mutableMapOf<Int, AtomicInteger>()
-        input.map { it.extractNumbers() }.forEach { (lt, rt) ->
-            left += lt
-            right.getOrPut(rt) { AtomicInteger() }.incrementAndGet()
-        }
-        return left.sumOf { it * (right[it]?.get() ?: 0) }
+        val (left, right) = input.map { it.extractNumbers() }.map { (lt, rt) -> lt to rt }.unzip()
+        val rightOccs = right.groupingBy { it }.eachCount()
+        return left.sumOf { it * (rightOccs[it] ?: 0) }
     }
 }
