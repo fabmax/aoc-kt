6501b3bdaa29d9d1db4db55af8dae916bdd89c83
fabmax <fabmax.thiele@gmail.com>
2024-12-08 13:53:42 +0100

    day 8

---
+++ src/main/kotlin/y2024/day08/Day08.kt
@@ -0,0 +1,41 @@
+package y2024.day08
+
+import AocPuzzle
+import de.fabmax.kool.math.Vec2i
+
+fun main() = Day08.runAll()
+
+object Day08 : AocPuzzle<Int, Int>() {
+
+    override fun solve1(input: List<String>): Int = solve(input, false)
+    override fun solve2(input: List<String>): Int = solve(input, true)
+
+    private fun solve(input: List<String>, multiples: Boolean): Int {
+        val width = input[0].length
+        val height = input.size
+        val antennaPositions = input
+            .flatMapIndexed { y, line -> line.mapIndexed { x, char -> char to Vec2i(x, y) } }
+            .groupBy { it.first }
+            .filterKeys { it != '.' }
+            .mapValues { (_, v) -> v.map { it.second } }
+
+        return antennaPositions.values.flatMap { positions ->
+            positions.indices
+                .flatMap { i -> positions.indices.map { j -> i to j } }
+                .filter { it.first != it.second }
+                .map { positions[it.first] to positions[it.second] }
+                .flatMap { (posA, posB) ->
+                    val d = posA - posB
+                    if (multiples) {
+                        generateSequence(posA) { pos -> pos + d }.takeWhile { it.isInLimits(width, height) }
+                    } else {
+                        sequenceOf(posA + d)
+                    }
+                }
+        }.filter { it.isInLimits(width, height) }.distinct().count()
+    }
+
+    private fun Vec2i.isInLimits(width: Int, height: Int): Boolean {
+        return x in 0 until width && y in 0 until height
+    }
+}
