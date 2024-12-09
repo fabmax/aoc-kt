1229a943cfdcc9a379a6d6445f256b000b0a46af
Max Thiele <fabian.thiele@fokus.fraunhofer.de>
2024-12-09 14:51:38 +0100

    slightly cleaned up day 9

--- src/main/kotlin/y2024/day09/Day09.kt
+++ src/main/kotlin/y2024/day09/Day09.kt
@@ -4,45 +4,61 @@ import AocPuzzle
 
 fun main() = Day09.runAll()
 
+data class FileInfo(val blockI: Int, val size: Int, val fileName: Int)
+
 object Day09 : AocPuzzle<Long, Long>() {
+
     override fun solve1(input: List<String>): Long {
-        val numbers = input[0].toCharArray().map { it.toString().toInt() }
-        val blocks = IntArray(numbers.sum()) { -1 }
+        val chunkSizes = input[0].toCharArray().map { it.toString().toInt() }
+        val memory = IntArray(chunkSizes.sum()) { -1 }
 
         var blockI = 0
-        var fileI = 0
-        for (i in numbers.indices) {
-            val isFile = i % 2 == 0
-            if (isFile) {
-                for (j in 0 until numbers[i]) {
-                    blocks[blockI + j] = fileI
-                }
-                fileI++
+        chunkSizes.forEachIndexed { chunkI, size ->
+            if (chunkI % 2 == 0) {
+                repeat(size) { memory[blockI + it] = chunkI / 2 }
             }
-            blockI += numbers[i]
+            blockI += size
         }
 
-        var freePtr = blocks.nextFree(0)
-        var lastFilePtr = blocks.prevFile(blocks.lastIndex)
-
+        var freePtr = memory.firstFree(0)
+        var lastFilePtr = memory.lastFile(memory.lastIndex)
         while (lastFilePtr > freePtr) {
-            blocks[freePtr] = blocks[lastFilePtr]
-            blocks[lastFilePtr] = -1
-            freePtr = blocks.nextFree(0)
-            lastFilePtr = blocks.prevFile(blocks.lastIndex)
+            memory[freePtr] = memory[lastFilePtr]
+            memory[lastFilePtr--] = -1
+            freePtr = memory.firstFree(freePtr)
         }
 
-        var checksum = 0L
-        for (i in blocks.indices) {
-            val f = blocks[i]
-            if (f > 0) {
-                checksum += f * i
-            }
-        }
-        return checksum
+        return memory.checksum()
     }
 
-    fun IntArray.nextFree(from: Int, freeSize: Int = 1): Int {
+    override fun solve2(input: List<String>): Long {
+        val chunkSizes = input[0].toCharArray().map { it.toString().toInt() }
+        val memory = IntArray(chunkSizes.sum()) { -1 }
+        val fileInfos = mutableListOf<FileInfo>()
+
+        var blockI = 0
+        chunkSizes.forEachIndexed { chunkI, size ->
+            if (chunkI % 2 == 0) {
+                fileInfos += FileInfo(blockI, size, 0)
+                repeat(size) { memory[blockI + it] = chunkI / 2 }
+            }
+            blockI += size
+        }
+
+        fileInfos.reversed().forEach { fileInfo ->
+            val freePtr = memory.firstFree(0, fileInfo.size)
+            if (freePtr > 0 && freePtr < fileInfo.blockI) {
+                repeat(fileInfo.size) { j ->
+                    memory[freePtr + j] = memory[fileInfo.blockI + j]
+                    memory[fileInfo.blockI + j] = -1
+                }
+            }
+        }
+
+        return memory.checksum()
+    }
+
+    fun IntArray.firstFree(from: Int, freeSize: Int = 1): Int {
         for (i in from .. (size - freeSize)) {
             if ((0 until freeSize).all { this[i + it] == -1 }) {
                 return i
@@ -51,7 +67,7 @@ object Day09 : AocPuzzle<Long, Long>() {
         return -1
     }
 
-    fun IntArray.prevFile(from: Int): Int {
+    fun IntArray.lastFile(from: Int): Int {
         for (i in from downTo 0) {
             if (this[i] != -1) {
                 return i
@@ -60,39 +76,10 @@ object Day09 : AocPuzzle<Long, Long>() {
         return -1
     }
 
-    override fun solve2(input: List<String>): Long {
-        val numbers = input[0].toCharArray().map { it.toString().toInt() }
-        val blocks = IntArray(numbers.sum()) { -1 }
-        val fileIndices = mutableListOf<FileInfo>()
-
-        var blockI = 0
-        var fileI = 0
-        for (i in numbers.indices) {
-            val isFile = i % 2 == 0
-            if (isFile) {
-                fileIndices += FileInfo(blockI, numbers[i], fileI)
-                for (j in 0 until numbers[i]) {
-                    blocks[blockI + j] = fileI
-                }
-                fileI++
-            }
-            blockI += numbers[i]
-        }
-
-        for (i in fileIndices.indices.reversed()) {
-            val info = fileIndices[i]
-            val freePtr = blocks.nextFree(0, info.size)
-            if (freePtr > 0 && freePtr < info.blockI) {
-                for (j in 0 until info.size) {
-                    blocks[freePtr + j] = info.fileI
-                    blocks[info.blockI + j] = -1
-                }
-            }
-        }
-
+    fun IntArray.checksum(): Long {
         var checksum = 0L
-        for (i in blocks.indices) {
-            val f = blocks[i]
+        for (i in indices) {
+            val f = this[i]
             if (f > 0) {
                 checksum += f * i
             }
@@ -100,5 +87,3 @@ object Day09 : AocPuzzle<Long, Long>() {
         return checksum
     }
 }
-
-data class FileInfo(val blockI: Int, val size: Int, val fileI: Int)
