898efd6dd06cc54bafa6eb409d25db08bf4836b8
Max Thiele <fabian.thiele@fokus.fraunhofer.de>
2024-12-20 11:20:42 +0100

    dijkstra is not actually needed

--- src/y2024/day20/Day20.kt
+++ src/y2024/day20/Day20.kt
@@ -3,11 +3,8 @@ package y2024.day20
 import AocPuzzle
 import coordSequence
 import de.fabmax.kool.math.Vec2i
-import extractNumbers
 import manhattanDistance
 import neighbors
-import kotlin.math.abs
-import kotlin.math.max
 
 fun main() = Day20.runAll()
 
@@ -26,10 +23,9 @@ object Day20 : AocPuzzle<Int, Int>() {
         val width = input[0].length
         val height = input.size
         val from = coordSequence(width, height).first { input[it] == 'S' }
-        val to = coordSequence(width, height).first { input[it] == 'E' }
 
         val map = input.map { it.replace('S', '.').replace('E', '.') }
-        val nodes = dijkstra(map, from, to)
+        val nodes = path(map, from)
 
         return nodes.values.sumOf { start ->
             start.pos.neighborhoodCoords(maxCheatLen, width, height)
@@ -42,30 +38,17 @@ object Day20 : AocPuzzle<Int, Int>() {
         }
     }
 
-    private fun dijkstra(grid: List<String>, from: Vec2i, to: Vec2i): Map<Vec2i, PathNode> {
-        val open = mutableSetOf(PathNode(from, 0))
-        val visited = mutableMapOf<Vec2i, PathNode>()
-
-        while (open.isNotEmpty()) {
-            val curr = open.first().also { open.remove(it) }
-            if (curr.distance >= (visited[curr.pos]?.distance ?: Int.MAX_VALUE)) {
-                continue
-            }
-            visited[curr.pos] = curr
-
-            curr.pos.neighbors()
-                .filter { grid[it] == '.' }
-                .map { PathNode(it, curr.distance + 1) }
-                .filter { it.distance < (visited[it.pos]?.distance ?: Int.MAX_VALUE) }
-                .forEach { open += it }
-        }
-
-        return visited
+    private fun path(grid: List<String>, from: Vec2i): Map<Vec2i, PathNode> {
+        val startNode = PathNode(from, 0)
+        return generateSequence(startNode to startNode) { (pp, p) ->
+            val next = p.pos.neighbors().find { it != pp.pos && grid[it] == '.' }
+            next?.let { p to PathNode(it, p.distance + 1) }
+        }.map { it.second }.associateBy { it.pos }
     }
 
     data class PathNode(val pos: Vec2i, val distance: Int)
 
-    fun Vec2i.neighborhoodCoords(maxDist: Int, width: Int, height: Int): Sequence<Vec2i> {
+    private fun Vec2i.neighborhoodCoords(maxDist: Int, width: Int, height: Int): Sequence<Vec2i> {
         val fromX = (x - maxDist).coerceAtLeast(1)
         val fromY = (y - maxDist).coerceAtLeast(1)
         val toX = (x + maxDist).coerceAtMost(width - 2)
