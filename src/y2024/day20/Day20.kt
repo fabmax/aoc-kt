62e182707d69c2f69d5b566229c1fd91e2d07706
Max Thiele <fabian.thiele@fokus.fraunhofer.de>
2024-12-20 07:09:36 +0100

    day 20

---
+++ src/y2024/day20/Day20.kt
@@ -0,0 +1,73 @@
+package y2024.day20
+
+import AocPuzzle
+import coordSequence
+import de.fabmax.kool.math.Vec2i
+import extractNumbers
+import manhattanDistance
+import neighbors
+import kotlin.math.abs
+
+fun main() = Day20.runAll()
+
+object Day20 : AocPuzzle<Int, Int>() {
+    override fun solve1(input: List<String>): Int {
+        val saves = if (isTestRun()) 20 else 100
+        return countCheats(input, 2, saves)
+    }
+
+    override fun solve2(input: List<String>): Int {
+        val saves = if (isTestRun()) 72 else 100
+        return countCheats(input, 20, saves)
+    }
+
+    private fun countCheats(input: List<String>, maxCheatLen: Int, savedPicos: Int): Int {
+        val width = input[0].length
+        val height = input.size
+        val from = coordSequence(width, height).first { input[it] == 'S' }
+        val to = coordSequence(width, height).first { input[it] == 'E' }
+
+        val map = input.map { it.replace('S', '.').replace('E', '.') }
+        val nodes = dijkstra(map, from, to)
+
+        var cnt = 0
+        coordSequence(width, height).filter { it in nodes }.forEach { startCoord ->
+            val startDist = nodes[startCoord]!!.distance
+            coordSequence(width, height)
+                .filter { it in nodes && it.manhattanDistance(startCoord) <= maxCheatLen }
+                .forEach { endCoord ->
+                    val endDist = nodes[endCoord]!!.distance
+                    val save = endDist - startDist - startCoord.manhattanDistance(endCoord)
+                    if (save >= savedPicos) {
+                        cnt++
+                    }
+                }
+        }
+        return cnt
+    }
+
+    private fun dijkstra(grid: List<String>, from: Vec2i, to: Vec2i): Map<Vec2i, PathNode> {
+        val open = mutableSetOf(PathNode(from, 0))
+        val visited = mutableMapOf<Vec2i, PathNode>()
+
+        while (open.isNotEmpty()) {
+            val curr = open.first().also { open.remove(it) }
+            if (curr.distance >= (visited[curr.pos]?.distance ?: Int.MAX_VALUE)) {
+                continue
+            }
+            visited[curr.pos] = curr
+
+            curr.pos.neighbors()
+                .filter { grid[it] == '.' }
+                .map { PathNode(it, curr.distance + 1) }
+                .filter { it.distance < (visited[it.pos]?.distance ?: Int.MAX_VALUE) }
+                .forEach { open += it }
+        }
+
+        return visited
+    }
+
+    data class PathNode(val pos: Vec2i, val distance: Int)
+
+    operator fun List<String>.get(pos: Vec2i): Char? = getOrNull(pos.y)?.getOrNull(pos.x)
+}
